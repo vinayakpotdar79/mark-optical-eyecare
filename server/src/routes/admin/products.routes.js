50717a864e597419f186fa399ba6c7738538aca1
Nikhil-2x <nikhilyadav101513@gmail.com>
2026-02-19 23:16:33 +0530

    add-route

--- server/src/routes/admin/products.routes.js
+++ server/src/routes/admin/products.routes.js
@@ -4,12 +4,14 @@ import {
   deleteProduct,
   getProductBySlug,
   getAllProducts,
+  updateProduct,
 } from "../../controllers/admin/products.controller.js";
 import upload from "../../middlewares/multer.js";
 
 const router = Router();
 
 router.post("/products", upload.array("images", 5), createProduct);
+router.patch("/products/:slug", upload.array("images", 5), updateProduct);
 router.get("/products/:slug", getProductBySlug);
 router.get("/products", getAllProducts);
 router.delete("/products/:slug", deleteProduct);
